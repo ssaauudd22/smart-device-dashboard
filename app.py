b0f0932c2eafc704ef90e018c11a6fcea2a6c82b
saud <saud.manganhar01@utrgv.edu>
2025-10-03 12:54:07 -0500

    Created visuals for data and linked backend with frontend

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -8,13 +8,12 @@ def home():
 
 @app.route("/usage")
 def usage():
-    data = {
-        "device": "printer 001",
-        "pages_printed": 120,
-        "ink_level": 65,
-        "energy_used_kwh": 3.4
-    }
-    return jsonify(data)
+    data = [
+        {"device": "printer 001", "pages_printed": 147, "ink_level": 35},
+        {"device": "printer 002", "pages_printed": 60, "ink_level": 83},
+        {"device": "Scanner 001", "scans": 25, "energy_used": 1.9}
+    ]
+    return jsonify({"data": data})
 
 if __name__ == "__main__":
     app.run(debug=True)
